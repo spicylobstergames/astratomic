90c4f5612a31f9f433ed9675c5e5c3e77bfe5e90
Isaac Turci <78173025+Zac8668@users.noreply.github.com>
2023-12-30 04:13:24 -0300

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,14 @@ mod geom_tools;
 mod manager_api;
 mod player;
 mod prelude {
+    pub use crate::atom::State;
     pub use crate::{
-        actors::*, animation::*, atom::*, chunk::*, chunk_group::*, chunk_manager::*,
-        consts::*, debug::*, geom_tools::*, manager_api::*, player::*,
+        actors::*, animation::*, atom::*, chunk::*, chunk_group::*, chunk_manager::*, consts::*,
+        debug::*, geom_tools::*, manager_api::*, player::*,
     };
     pub use bevy::math::{ivec2, ivec3, uvec2, uvec3, vec2, vec3};
     pub use bevy::prelude::*;
     pub use std::collections::{HashMap, HashSet};
-    pub use crate::atom::State;
 }
 
 use crate::animation::AnimationPlugin;
